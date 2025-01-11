e2ed8e719551bdfa3d9bdbafd196865aa3a68091
Paulo Henrique Ferreira França <162806228+PauloHenrique993940@users.noreply.github.com>
2025-01-11 15:38:01 -0300

    Fim

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,21 @@
-const gulp = require('gulp')(require('gulp'));
-const imagemin = require('gulp-imagemin'); // Importa corretamente o gulp-imagemin
-const sass = require('gulp-sass')(require('sass')); // Importa o gulp-sass
-const uglify = require('gulp-uglify'); // Importa o gulp-uglify
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin'); // Certifique-se de instalar o gulp-imagemin: npm install gulp-imagemin --save-dev
 
-// Função para processar os arquivos SASS
 function styles() {
     return gulp.src('./src/styles/*.scss')
-        .pipe(sass({ outputStyle: 'compressed' })) // Comprime o CSS
-        .pipe(gulp.dest('./dist/css')); // Salva na pasta dist/css
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(gulp.dest('./dist/css'));
 }
 
-// Função para otimizar imagens
 function images() {
     return gulp.src('./src/images/**/*')
-        .pipe(imagemin()) // Aplica o plugin gulp-imagemin
-        .pipe(gulp.dest('./dist/images')); // Salva na pasta dist/images
+        .pipe(imagemin()) // Adicione o "."
+        .pipe(gulp.dest('./dist/images'));
 }
 
-// Exporta as tarefas padrão
 exports.default = gulp.parallel(styles, images);
-
-// Exporta a tarefa de observação
-exports.watch = function () {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+exports.watch = function() {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles)); // Corrigido o erro no nome do arquivo
+    gulp.watch('./src/images/**/*', gulp.parallel(images)); // Adicionado para monitorar imagens
 };

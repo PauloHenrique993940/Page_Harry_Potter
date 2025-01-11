bd53fb0e2bfa5b920a191511d89700465c38630a
Paulo Henrique Ferreira França <162806228+PauloHenrique993940@users.noreply.github.com>
2025-01-11 15:16:41 -0300

    Corrigindo erros

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,26 @@
-import gulp from 'gulp';
-import sass from 'gulp-sass';
-import dartSass from 'sass';
-import imagemin from 'gulp-imagemin';
+const gulp = require('gulp');
+const imagemin = require('gulp-imagemin'); // Importa corretamente o gulp-imagemin
+const sass = require('gulp-sass')(require('sass')); // Importa o gulp-sass
+const uglify = require('gulp-uglify'); // Importa o gulp-uglify
 
-sass.compiler = dartSass;
-
-// Função para processar arquivos SCSS
+// Função para processar os arquivos SASS
 function styles() {
-    return gulp.src('./src/styles/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./dist/styles'));
+    return gulp.src('./src/styles/*.scss')
+        .pipe(sass({ outputStyle: 'compressed' })) // Comprime o CSS
+        .pipe(gulp.dest('./dist/css')); // Salva na pasta dist/css
 }
 
 // Função para otimizar imagens
 function images() {
     return gulp.src('./src/images/**/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('./dist/images'));
+        .pipe(imagemin()) // Aplica o plugin gulp-imagemin
+        .pipe(gulp.dest('./dist/images')); // Salva na pasta dist/images
 }
 
-// Tarefa padrão com watch
-function defaultTask() {
-    gulp.watch('./src/styles/**/*.scss', styles);
-    gulp.watch('./src/images/**/*', images);
-}
+// Exporta as tarefas padrão
+exports.default = gulp.parallel(styles, images);
 
-export { styles, images, defaultTask as default };
+// Exporta a tarefa de observação
+exports.watch = function () {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+};

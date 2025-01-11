77f9bbd31f7900e24f8721e49949e84c9184b9c3
Paulo Henrique Ferreira França <162806228+PauloHenrique993940@users.noreply.github.com>
2025-01-11 14:24:05 -0300

    Enviando setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-
+const imagemin = require('gulp-imagemin'); // Certifique-se de instalar o gulp-imagemin: npm install gulp-imagemin --save-dev
 
 function styles(){
-    return gulp.src('./src/styles/*.scss')
-        .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(gulp.dest('./dist/css'))
+    return gulp.src('./src/images/**/*')
+    .pipe(imagemin()) // Adicione o "."
+    .pipe(gulp.dest('./dist/images'));
+
+}
+
+function images(){
+    return gulp.src('./src/images/**/*')
+    pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'));
 }
 
 
@@ -18,7 +25,8 @@ function styles(){
 
 
 
-exports.default = styles;
+exports.default = gulp.parallel(styles, images, scripts);
 exports.watch = function() {
-    gulp.watch('./src/styles/*.scsss', gulp.parallel(styles))
+    gulp.watch('./src/styles/*.scsss',  gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
